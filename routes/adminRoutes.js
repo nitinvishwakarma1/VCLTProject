6635ed6a577e35717048978469cc942440e8f129
unknown <abc@gmail.com>
2024-02-01 13:00:15 +0530

    My Initial Commit

---
+++ routes/adminRoutes.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { adminViewUserController, adminDeactivateUser , adminViewFeedbackController , adminViewRequestController } from '../controller/adminController.js';
+
+const adminRouter = express.Router();
+
+adminRouter.get('/', (req, res) => {
+    res.render('pages/adminmainpage');
+});
+
+adminRouter.get('/adminViewUser', adminViewUserController);
+adminRouter.get('/deactivate/:email', adminDeactivateUser);
+adminRouter.get('/adminViewRequest', adminViewRequestController);
+adminRouter.get('/adminViewFeedbacks', adminViewFeedbackController);
+
+export default adminRouter;
